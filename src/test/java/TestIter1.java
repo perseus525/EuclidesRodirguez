b555257ef13a1c2ba28b5f894695eb5619afe27c
perseus525 <37369372+perseus525@users.noreply.github.com>
2018-05-18 10:16:26 -0400

    Se agrega test unitario de version iterativa

---
+++ src/test/java/TestIter1.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author marclo
+ */
+public class TestIter1 {
+    
+    public TestIter1() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+ public void test3Ciencias1(){
+
+     
+     assertEquals(21 , Iter.mcd(756,525));
+     assertEquals(12 , Iter.mcd(1032,180));
+     
+
+}
+}
